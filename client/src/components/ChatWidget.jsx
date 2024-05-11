e0c335f788eb57478a98b1552e5a32a3724d20ee
sansRebel <khaledalsinafi101@gmail.com>
2024-05-11 16:53:15 +0800

    Update to access correct server url

--- client/src/components/ChatWidget.jsx
+++ client/src/components/ChatWidget.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const apiBaseUrl = 'http://localhost:3001';
+const apiBaseUrl = 'https://testing-app-server.vercel.app/';
 console.log(apiBaseUrl);
 
 function ChatWidget() {
