1b974c217f9c520db9131e6161c2e87c7942ec36
sansRebel <khaledalsinafi101@gmail.com>
2024-05-12 20:03:27 +0800

    css

--- client/src/components/ChatWidget.jsx
+++ client/src/components/ChatWidget.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './ChatWidget.css';
 
 const apiBaseUrl = 'https://testing-app-server.vercel.app';
 console.log(apiBaseUrl);
