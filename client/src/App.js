7336a5e86e7ef39876b305497794ffb62ec17702
sansRebel <khaledalsinafi101@gmail.com>
2024-05-12 20:05:39 +0800

    css

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
-            <h1>Welcome to the Chat App</h1>
+            <h1>Welcome to the flight booking app</h1>
         </header>
         <ChatWidget />
     </div>
