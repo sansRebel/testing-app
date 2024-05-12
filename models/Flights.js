4662586b595078e48ba3443232208f2675258e01
sansRebel <khaledalsinafi101@gmail.com>
2024-05-12 16:31:13 +0800

    update to connection in F

--- models/Flights.js
+++ models/Flights.js
@@ -1,9 +1,7 @@
 // models/Flight.js
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+
 
 const flightSchema = new mongoose.Schema({
   flightNumber: String,
